71a043716b64f18d08c5cf42bd7234a684974faf
abashkatov <as@bashkatov.pro>
2020-09-16 22:33:53 +0300

    Добавляет энтити менеджер и класс сущности Person

---
+++ ConsoleDBApp/Entity/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ConsoleDBApp.Entity
+{
+    [Table("app_persons")]
+    class Person
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("phone")]
+        public string Phone { get; set; }
+    }
+}
