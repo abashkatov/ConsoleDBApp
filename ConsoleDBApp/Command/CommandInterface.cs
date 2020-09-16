c9426a18a9e96882a4edd7635b9fb3ece2055ce2
abashkatov <as@bashkatov.pro>
2020-09-16 22:46:09 +0300

    Добавляет интерфейс для команд

---
+++ ConsoleDBApp/Command/CommandInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleDBApp.Command
+{
+    interface CommandInterface
+    {
+        public bool CanRun(string input);
+        public string Run(string input, ref bool isExit);
+        public string GetMenuRow();
+    }
+}
