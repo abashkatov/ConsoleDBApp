09c0020f6778ddfa743964e2eae738c32f292ad3
abashkatov <as@bashkatov.pro>
2020-09-16 22:54:31 +0300

    Добавляет команду для добавления новых записей

---
+++ ConsoleDBApp/Command/AddPersonCommand.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleDBApp.Context;
+using ConsoleDBApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace ConsoleDBApp.Command
+{
+    class AddPersonCommand : CommandInterface
+    {
+        private readonly AppDbContext context;
+
+        public AddPersonCommand(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public bool CanRun(string input) => input == "add";
+
+        public string GetMenuRow() => "add - добавить новую запись";
+
+        public string Run(string input, ref bool isExit)
+        {
+            Person person = new Person();
+            Write("Введите имя: ");
+            person.FirstName = ReadLine();
+            Write("Введите фамилию: ");
+            person.LastName= ReadLine();
+            Write("Введите телефон: ");
+            person.Phone = ReadLine();
+
+            context.Persons.Add(person);
+            context.SaveChanges();
+
+            return "Новая запись была добавлена";
+        }
+    }
+}
