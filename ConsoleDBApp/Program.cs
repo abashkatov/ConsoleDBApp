02d1583f5e056b8ed0182da6b14137f922ce5d34
abashkatov <as@bashkatov.pro>
2020-09-16 23:19:08 +0300

    Добавляет основную логику приложения: - конфигурирование приложения, в т.ч. сборка массива доступных пользователю команд, - основной цикл работы приложения с взаимодействием с пользователем и выполнением команд

--- ConsoleDBApp/Program.cs
+++ ConsoleDBApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ConsoleDBApp.Command;
+using ConsoleDBApp.Context;
+using System;
+using static System.Console;
+using System.Collections.Generic;
 
 namespace ConsoleDBApp
 {
@@ -6,7 +10,32 @@ namespace ConsoleDBApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AppDbContext context = new AppDbContext();
+            List<CommandInterface> commands = new List<CommandInterface>();
+            commands.Add(new ExitCommand());
+            commands.Add(new AddPersonCommand(context));
+            commands.Add(new GetPersonListCommand(context));
+            bool isExit = false;
+            string input;
+            string commandResult;
+            do {
+                WriteLine("===========================================");
+                foreach (CommandInterface command in commands) {
+                    WriteLine(command.GetMenuRow());
+                }
+                WriteLine();
+                Write("Введите команду: ");
+                input = ReadLine();
+                foreach (CommandInterface command in commands)
+                {
+                    if (command.CanRun(input)) {
+                        commandResult = command.Run(input, ref isExit);
+                        WriteLine(commandResult);
+                        break;
+                    }
+                }
+            } while (!isExit);
+            ReadKey();
         }
     }
 }
