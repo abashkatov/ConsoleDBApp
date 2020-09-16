1eea2f2d317ea0d926c388ac2c36c2a16ba7ad8e
abashkatov <as@bashkatov.pro>
2020-09-16 23:10:18 +0300

    Добавляет команду для вывода списка всех записей

---
+++ ConsoleDBApp/Command/GetPersonListCommand.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleDBApp.Context;
+using ConsoleDBApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleDBApp.Command
+{
+    class GetPersonListCommand : CommandInterface
+    {
+        private readonly AppDbContext context;
+
+        public GetPersonListCommand(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public bool CanRun(string input) => input == "list";
+
+        public string GetMenuRow() => "list - вывод всех записей";
+
+        public string Run(string input, ref bool isExit)
+        {
+            StringBuilder result = new StringBuilder();
+            string separator = "|-----|------------|------------|-----------|";
+
+            WriteLine(separator);
+            WriteLine("|   Id| First Name |  Last Name |     Phone |");
+            WriteLine(separator);
+            foreach (Person person in context.Persons)
+            {
+                WriteLine($"|{person.Id,5}|{person.FirstName,12}|{person.LastName,12}|{person.Phone,11}|");
+            }
+            WriteLine(separator);
+
+            return result.ToString();
+        }
+    }
+}
