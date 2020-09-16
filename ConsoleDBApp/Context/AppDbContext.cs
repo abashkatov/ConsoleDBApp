c45e639efa4aa1d80f380272777e5ea3f90795ba
abashkatov <as@bashkatov.pro>
2020-09-16 22:37:12 +0300

    Добавляет класс контекста для связи с БД и одной таблицей Person

---
+++ ConsoleDBApp/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleDBApp.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleDBApp.Context
+{
+    class AppDbContext: DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=db.db");
+    }
+}
