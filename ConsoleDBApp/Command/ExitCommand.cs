b98b74618334c51ab82b3aee343abe398f94f926
abashkatov <as@bashkatov.pro>
2020-09-16 22:48:28 +0300

    Добавляет команду для обработки выхода из приложения

---
+++ ConsoleDBApp/Command/ExitCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleDBApp.Command
+{
+    class ExitCommand : CommandInterface
+    {
+        public bool CanRun(string input)
+        {
+            return input == "exit";
+        }
+
+        public string GetMenuRow()
+        {
+            return "exit - выход";
+        }
+
+        public string Run(string input, ref bool isExit)
+        {
+            isExit = true;
+            return "Работа приложения завершена.";
+        }
+    }
+}
